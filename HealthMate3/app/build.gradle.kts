61879b2cd06977b1c01f761cdfd66443b3536649
AmanPSingh <amanp.singh0210@gmail.com>
2025-04-01 12:42:23 +0530

    .

--- HealthMate3/app/build.gradle.kts
+++ HealthMate3/app/build.gradle.kts
@@ -75,6 +75,11 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0'")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.9.1")
+
+
     // Multidex support
     implementation("androidx.multidex:multidex:2.0.1")
     
